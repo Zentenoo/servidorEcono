f967ce3e5daee4b2297c7e8170b2b9d9bd4ee714
mateo.z <mateo.z@tugerente.com>
2024-09-06 19:03:24 -0400

    primer commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { chromium } = require('playwright'); // Ajuste de importación
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/compraBob', async (req, res) => {
+    try {
+        const browser = await chromium.launch({ headless: true });
+        const page = await browser.newPage();
+        await page.goto('https://p2p.binance.com/trade/all-payments/USDT?fiat=BOB');
+        await page.waitForSelector('.headline5.mr-4xs.text-primaryText', { state: 'visible' });
+        const products = await page.$$eval(
+            '.headline5.mr-4xs.text-primaryText', 
+            (results) => results.map((el) => {
+                const precio = el.innerText;
+                return { precio };
+            })
+        );
+        console.log(products);
+        await browser.close();
+        res.json(products); 
+    } catch (error) {
+        console.error('Error al hacer scraping:', error);
+        res.status(500).send('Error al hacer scraping');
+    }
+});
+
+app.get('/ventaArs', async (req, res) => {
+    try {
+        const browser = await chromium.launch({ headless: true });
+        const page = await browser.newPage();
+        await page.goto('https://p2p.binance.com/trade/sell/USDT?fiat=ARS&payment=all-payments');
+        await page.waitForSelector('.headline5.mr-4xs.text-primaryText', { state: 'visible' });
+        const products = await page.$$eval(
+            '.headline5.mr-4xs.text-primaryText', 
+            (results) => results.map((el) => {
+                const precio = el.innerText;
+                return { precio };
+            })
+        );
+        console.log(products);
+        await browser.close();
+        res.json(products); 
+    } catch (error) {
+        console.error('Error al hacer scraping:', error);
+        res.status(500).send('Error al hacer scraping');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
