4e39fbe7b46fc3a8996e6fccbffd8265ce2cbb4a
mateo.z <mateo.z@tugerente.com>
2024-09-11 01:49:50 -0400

    cors añadido

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 
 app.post('/api/binance', async (req, res) => {
   try {
